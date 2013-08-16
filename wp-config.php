9b0f515a2cee9b9b921be67dc7f78356b9121668
Adam Baker <abaker.mail@gmail.com>
2013-08-16 01:16:24 -0600

    Second commit. Basic Test.

--- wp-config.php
+++ wp-config.php
@@ -16,7 +16,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'localwp');
+define('DB_NAME', 'townwp');
 
 /** MySQL database username */
 define('DB_USER', 'wordpress');
